74930bfbc5c2be0797b557646723d779adf05fd8
Roman Stoffel <roman.stoffel@gamlor.info>
2014-10-14 00:10:47 +0900

    Testing trigger by maven repo

--- build.bat
+++ build.bat
@@ -1,7 +1,8 @@
 IF NOT DEFINED SPN set SPN=spoon
 IF "%1" == "" (
-   echo "Expect a version. Use this command like: build version. Example build 9.2.3.v20140905"
-   exit /b 1
+   echo "Picking up version from autobuild.version.txt. You can overwrite the version by calling build.bat 9.2.3.v20140905"
+   SET /p VERSION=<autobuild.version.txt
+) ELSE (
+	set VERSION=%1
 )
-set VERSION=%1
 %SPN% build --name=jetty:%VERSION% spoon.me
